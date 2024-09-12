f6be4b992e2b3f89d955c2d31d9302dd428e97c3
AniHo <anihox028@ihmeducation.se>
2024-09-12 14:42:55 +0200

    app2

---
+++ P5Operators/js/app2.js
@@ -0,0 +1,60 @@
+let firstNumber = 7;
+let secondNumber = 2;
+let answer = 0;
+
+// 1+2 = 3
+
+answer = firstNumber + secondNumber ;
+console.log(answer);
+
+answer = firstNumber - secondNumber ;
+console.log(answer);
+
+answer = firstNumber * secondNumber ;
+console.log(answer);
+
+answer = firstNumber % secondNumber ;
+console.log(answer);
+
+let gold = 50;
+gold+10;
+console.log(gold);
+
+gold = gold + 10;
+console.log(gold);
+
+gold +=10;
+console.log(gold);
+
+gold *=10;
+console.log(gold);
+
+gold /=10;
+console.log(gold);
+
+gold -=10;
+console.log(gold);
+
+gold /=10;
+console.log(gold);
+
+gold %=2;
+console.log(gold);
+
+let numberOfStudents = 0;
+
+// numberOfStudents += 1;
+numberOfStudents += 1;
+console.log(numberOfStudents);
+
+numberOfStudents++;
+console.log(numberOfStudents);
+
+++numberOfStudents;
+console.log(numberOfStudents);
+
+numberOfStudents -= 1;
+console.log(numberOfStudents);
+
+
+
