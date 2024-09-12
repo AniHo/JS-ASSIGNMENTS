60c1487049b2fd45a53e3da240037c28634535d9
AniHo <anihox028@ihmeducation.se>
2024-09-12 23:15:52 +0200

    Exercise2

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -17,10 +17,33 @@ let remainderOutput = 0;
 remainderOutput = firstNumber % secondNumber ;
 console.log(remainderOutput);
 
-let areaInput = 2 * 2;
+let areaInput = 2;
 let areaInput2 = 3.14;
-outputArea = areaInput2 * areaInput;
+areaInput1 = areaInput ** 2;
+outputArea = areaInput2 * areaInput1;
 console.log(outputArea);
+
+let negationInput = 11;
+let negationOutput = 0;
+negationOutput -= negationInput;
+console.log(negationOutput);
+
+let input1 = 70;
+let input2= 1.82
+input3 = input2 ** 2;
+console.log( input3);
+finalOutput = input1 / input3;
+console.log(finalOutput);
+
+let a = 3;
+let b = 4;
+
+c = a ** 2 + b ** 2;
+console.log(c);
+
+let hypotenuse = Math.sqrt(c);
+console.log(hypotenuse);
+
 
 
 
