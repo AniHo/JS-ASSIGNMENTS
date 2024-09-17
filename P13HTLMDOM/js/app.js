f32a6900544f03087a18eca00cc7ba8cc1e35b1c
AniHo <anihox028@ihmeducation.se>
2024-09-17 14:02:13 +0200

    P13

---
+++ P13HTLMDOM/js/app.js
@@ -0,0 +1,9 @@
+
+const textField0 = document.getElementById("text0");
+
+textField0.innerHTML = 'Anisa';
+const headerText5 = document.getElementById("header5");
+
+headerText5.innerHTML = "This is my Website";
+
+headerText5.style.color = "blue"
