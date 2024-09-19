5160a741d2faaa2b05150ee47f7f6172423d3788
AniHo <anihox028@ihmeducation.se>
2024-09-19 10:48:09 +0200

    refactore

--- P13HTML/js/app.js
+++ P13HTML/js/app.js
@@ -1,8 +1,24 @@
-const textField0  = document.getElementById("text0");
-textField0.innerHTML = showStylizedScore (786);
-function showScore (inputNumber) {
-  return inputNumber *100;
+//Document Elements
+//Looks for "text0"
+
+const textField0 = document.getElementById("text0");
+const textField1 = document.getElementById("text1");
+//Internal Variables
+let score =0;
+
+increaseScoreByOne();
+function showStylizedScore(scoreInput) {
+  return "Your Score is: " + scoreInput * 100;
 }
-  function showStylizedScore(scoreInput) {
-    return "Your score is " + scoreInput *100;
-  }
+//Controllers
+function increaseScoreByOne() {
+  score++
+  updateScoreText(score);
+}
+//View
+function updateScoreText(newText) {
+  textField0.innerHTML = newText;
+}
+function updateStatusText(newText) {
+  textField0.innerHTML = newText;
+}
