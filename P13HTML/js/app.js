60eab4d6d788cc7915e1761e434d11c02830a2ab
AniHo <anihox028@ihmeducation.se>
2024-09-19 11:26:50 +0200

    IncreaseButton

--- P13HTML/js/app.js
+++ P13HTML/js/app.js
@@ -3,22 +3,35 @@
 
 const textField0 = document.getElementById("text0");
 const textField1 = document.getElementById("text1");
+
+const increaseScoreButton  = document.getElementById("button0");
 //Internal Variables
 let score =0;
+let win = false;
+
+increaseScoreButton.addEventListener("click", () => {
+  increaseScoreByOne();
+  updateScoreText();
+  checkScoreForSeven();
+});
 
 increaseScoreByOne();
-function showStylizedScore(scoreInput) {
-  return "Your Score is: " + scoreInput * 100;
-}
+
 //Controllers
 function increaseScoreByOne() {
   score++
-  updateScoreText(score);
 }
+function checkScoreForSeven() {
+  if (score >=7) {
+    changeScoreTextColor();
+  }
+}
+
 //View
-function updateScoreText(newText) {
-  textField0.innerHTML = newText;
+function updateScoreText() {
+  textField0.innerHTML = "Your score is :" + score;
 }
-function updateStatusText(newText) {
-  textField0.innerHTML = newText;
+function changeScoreTextColor() {
+  textField0.style.color ="green";
 }
+
